0f3e4b4f5c46d71162475e14b2961703f2668b5e
Isa <IsabellaGreco@omrits-air.home>
2024-10-29 10:04:56 +0000

    added stripe and be

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    }
+  },
   server: {
     host: true,
     port: 5173
